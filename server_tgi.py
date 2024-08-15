e8c9e4c158c44099f5c6e068cb1dce54fdc7b670
kiranbeethoju <33856173+kiranbeethoju@users.noreply.github.com>
2024-08-15 22:19:29 +0530

    Update server_tgi.py

--- server_tgi.py
+++ server_tgi.py
@@ -49,27 +49,9 @@ process = subprocess.Popen(docker_command, shell=True)
 
 # Wait for the server to start (adjust the sleep time if needed)
 print("Waiting for TGI server to start.....")
-time.sleep(30)
+# time.sleep(30)
 
-# Set up TGI client
-tgi_address = "http://localhost:8080"
-print("Setting up TGI client.....")
-client = Client(tgi_address)
-
-print(f"Process start time is {datetime.now()}")
-st = datetime.now()
-
-# Generate text
-prompt = "Provide background on transformers."
-print("Generating text.....")
-output = client.generate(prompt, max_new_tokens=1024)
-
-print(output.generated_text)
-
-en = datetime.now()
-print(f"Process end time is {datetime.now()}")
-print(f"Total time is {en-st}")
 
 # Stop the Docker container
 print("Stopping TGI server.....")
-subprocess.run("docker stop $(docker ps -q --filter ancestor=ghcr.io/huggingface/text-generation-inference:latest)", shell=True)
+#subprocess.run("docker stop $(docker ps -q --filter ancestor=ghcr.io/huggingface/text-generation-inference:latest)", shell=True)
