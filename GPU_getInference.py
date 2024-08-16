64d3774cd2726a43e66cb1d55b3b7a1d02943c62
kiranbeethoju <33856173+kiranbeethoju@users.noreply.github.com>
2024-08-16 06:54:38 +0000

    Update GPU_getInference.py

--- GPU_getInference.py
+++ GPU_getInference.py
@@ -1,10 +1,10 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(base_url="http://0.0.0.0:8080/v1", api_key=os.getenv("OPENAI_API_KEY", "-"))
+client = OpenAI(base_url="http://0.0.0.0:8000/v1", api_key=os.getenv("VLLM_API_KEY", "-"))
 
 chat_completion = client.chat.completions.create(
-  model="tgi",
+  model="hugging-quants/Meta-Llama-3.1-70B-Instruct-GPTQ-INT4",
   messages=[
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": "What is Deep Learning?"},
